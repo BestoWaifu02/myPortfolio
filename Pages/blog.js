57ff5f48644b6c45b7a920adbc0a891fd98f5289
BestoWaifu02 <BestoWaifu02@gmail.com>
2021-03-03 20:27:58 -0600

    commit

---
+++ Pages/blog.js
@@ -0,0 +1,8 @@
+import Layout from "../components/Layout";
+const blog = () => (
+  <Layout>
+    <h1>Blog</h1>
+  </Layout>
+);
+
+export default blog;
---
+++ Pages/github.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+const github = () => (
+  <Layout>
+    <h1>Github</h1>
+  </Layout>
+);
+
+export default github;
